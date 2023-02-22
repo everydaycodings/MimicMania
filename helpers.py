c8ae39a90c68b232ee5344db9b6de165f2a57760
Kumar Saksham <everydaycodings@gmail.com>
2023-02-23 01:22:30 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -44,7 +44,6 @@ class DownloadModels():
             # Download the ZIP file to a temporary folder
             response = requests.get(url, stream=True)
             total_size_in_bytes = int(response.headers.get('content-length', 0))
-            st.write("yes")
             block_size = 1024  # 1 Kibibyte
             progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
 
